848051ed71a553a3ae2cfbbf45ed7c3efad15d75
Marlvin <j00975960@students.jsums.edu>
2025-11-21 08:47:36 -0600

    Add redirects from /blogs to /essays

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blogs',
+        destination: '/essays',
+        permanent: true,
+      },
+      {
+        source: '/blogs/:slug',
+        destination: '/essays/:slug',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
